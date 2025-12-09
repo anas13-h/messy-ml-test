cc8aaaefc25dddc30455b84853ddb8b5822c000a
Muhammad Anas <isthisanas987@gmail.com>
2025-12-09 08:22:33 +0500

    updated once again (shuru se shuru)

--- train.py
+++
@@ -1,7 +0,0 @@
-# train.py - Training script that shouldn't be deployed
-import torch
-from torch.utils.data import DataLoader
-
-# Heavy training dependencies
-# Large dataset loading
-# This will confuse the scanner
